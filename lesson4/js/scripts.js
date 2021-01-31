9ae6db7d6fd3e8fd6acf8eeb1caff4187ce74150
jessiemangum16 <jessiemangum16@byui.edu>
2021-01-31 11:25:30 -0700

    update

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -11,7 +11,7 @@ function lastModified() {
 
     var date = `${dOfWeek[d.getDay()]}, ${d.getDate()} ${month[d.getMonth()]} ${d.getFullYear()}`;
     var cYear = d.getFullYear();
-    var yearOutput = "&#169; " + cYear + "| Jessie Mangum | Utah |" + '<a href="https://www.byui.edu/online">BYUI Online Learning</a>';
+    var yearOutput = "&#169; " + cYear + "| Jessie Mangum | Utah | " + '<a href="https://www.byui.edu/online">BYUI Online Learning</a>';
 
     document.getElementById('cYear').innerHTML = yearOutput;
     document.getElementById('mod').innerHTML = date;
