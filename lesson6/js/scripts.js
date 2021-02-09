fca22fd117445233a2a4c3d5b8d7098778040ce6
jessiemangum16 <jessiemangum16@byui.edu>
2021-02-09 11:57:07 -0700

    update

--- lesson6/js/scripts.js
+++ lesson6/js/scripts.js
@@ -16,3 +16,17 @@ function lastModified() {
     document.getElementById('cYear').innerHTML = yearOutput;
     document.getElementById('mod').innerHTML = date;
 }
+
+function windChill(){
+
+    var tempF = 45;
+    var speed = 15;
+    var chill = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed,0.16)) + (0.4275 * tempF * Math.pow(speed,0.16));
+
+    chill = Math.round(chill * 1) / 1;
+
+    document.getElementById('windChill').innerHTML = chill;
+
+    document.getElementById('temp').innerHTML = tempF;
+    document.getElementById('windSpeed').innerHTML = speed;
+}
