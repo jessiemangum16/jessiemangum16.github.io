ac5ea958b71b861c4d0ff815bcf626c150a9e4fe
jessiemangum16 <jessiemangum16@byui.edu>
2021-02-23 17:25:33 -0700

    update

--- lesson8/js/scripts.js
+++ lesson8/js/scripts.js
@@ -74,4 +74,15 @@ if('IntersectionObserver' in window) {
     });
   }
 
+  function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
+
+function selectResponse() {
+	const s = document.querySelector('#selected')
+	const sel = document.querySelector('#selectRegion');
+	s.style.display = "block";
+	s.textContent = sel.value;
+	
+}
   
