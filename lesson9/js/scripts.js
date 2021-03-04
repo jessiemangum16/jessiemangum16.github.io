f9a072e2358d79a947913ce2fcb2a506681d2c50
jessiemangum16 <jessiemangum16@byui.edu>
2021-03-04 13:53:28 -0700

    update

--- lesson9/js/scripts.js
+++ lesson9/js/scripts.js
@@ -1,8 +1,10 @@
+/*************************** TOGGLE MENU ***************************/
 function toggleMenu(){
     console.log(document.getElementById("top-menu").classList);
     document.getElementById("top-menu").classList.toggle("hide");
 }
 
+/*************************** LAST MODIFIED/CURRENT YEAR ***************************/
 function lastModified() {
     var d = new Date();
 
@@ -17,6 +19,7 @@ function lastModified() {
     document.getElementById('mod').innerHTML = date;
 }
 
+/*************************** WIND CHILL ***************************/
 function windChill(){
 
     var tempF = 45;
@@ -31,6 +34,7 @@ function windChill(){
     document.getElementById('windSpeed').innerHTML = speed;
 }
 
+/*************************** SHOW TOP TEXT ON FRIDAY ***************************/
 function showFri() {
     var d = new Date();
 
@@ -41,6 +45,7 @@ function showFri() {
     }
 }
 
+/*************************** LAZY LOAD ***************************/
 let imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const loadImages = (image) => {
@@ -85,4 +90,45 @@ function selectResponse() {
 	s.textContent = sel.value;
 	
 }
-  
+
+/*************************** HOME PAGE TOWN DATA ***************************/
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const towns = jsonObject['towns'];
+    const townsA = [5, 1, 6]
+    for (let i = 0; i < townsA.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let h4 = document.createElement('h4');
+        let h3_1 = document.createElement('p');
+        let h3_2 = document.createElement('p');
+        let h3_3 = document.createElement('p');
+        let img = document.createElement('img');
+
+        h2.textContent = towns[townsA[i]].name;
+        h4.textContent = '"' + towns[townsA[i]].motto + '"';
+        h3_1.textContent = 'Year Founded: ' + towns[townsA[i]].yearFounded;
+        h3_2.textContent = 'Population: ' + towns[townsA[i]].currentPopulation;
+        h3_3.textContent = 'Annual Rainfall: ' + towns[townsA[i]].averageRainfall + '"';
+        img.setAttribute('src', 'images/' + towns[townsA[i]].photo);
+        img.setAttribute('alt', towns[townsA[i]].name + 'photo');
+
+        card.appendChild(h2);
+        card.appendChild(h4);
+        card.appendChild(h3_1);
+        card.appendChild(h3_2);
+        card.appendChild(h3_3);
+        card.appendChild(img);
+
+        document.querySelector('div.towns').appendChild(card);
+        
+
+        
+    }
+  });
