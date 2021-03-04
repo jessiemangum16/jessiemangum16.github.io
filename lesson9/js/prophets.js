6966d24df09d8f6827429b6cd227acfaa8dba333
jessiemangum16 <jessiemangum16@byui.edu>
2021-03-04 11:35:27 -0700

    update

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -18,6 +18,7 @@ fetch(requestURL)
         p.textContent = 'Date of Birth: ' + prophets[i].birthdate;
         p2.textContent = 'Place of Birth: ' + prophets[i].birthplace;
         image.setAttribute('src', prophets[i].imageurl);
+        image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname);
 
         card.appendChild(h2);
         card.appendChild(p);
