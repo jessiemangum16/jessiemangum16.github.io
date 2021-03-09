33667a9c133cf98bc29853c72d47d1b732e4a500
jessiemangum16 <jessiemangum16@byui.edu>
2021-03-08 20:06:42 -0700

    update

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=159b64909a20d12c8a9f4243af9f627b";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=159b64909a20d12c8a9f4243af9f627b";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
