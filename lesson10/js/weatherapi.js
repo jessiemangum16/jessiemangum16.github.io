e51d3295a6ed0fcc9ccf0c06c591e61e67c8dca4
jessiemangum16 <jessiemangum16@byui.edu>
2021-03-08 20:57:00 -0700

    update

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -3,11 +3,21 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
+
+    // TEST SITE
+    let prestonTemp = (jsObject.main.temp - 273.15) * (9/5) + 32;
+    
+    document.getElementById('current-temp').textContent = prestonTemp.toFixed(0);
 
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
     const desc = jsObject.weather[0].description;  // note how we reference the weather array
     document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
     document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
     document.getElementById('icon').setAttribute('alt', desc);
+
+    //WEATHER SITE
+    document.getElementById('current').textContent = jsObject.main.temp;
+    document.getElementById('high').textContent = jsObject.main.temp_max;
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('windSpeed').textContent = jsObject.wind.speed;
   });
