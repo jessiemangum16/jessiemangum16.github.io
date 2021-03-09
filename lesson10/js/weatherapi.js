168b1f090e7e5bcfbf828877dc415c5cecf367bd
jessiemangum16 <jessiemangum16@byui.edu>
2021-03-08 20:05:07 -0700

    update

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,8 +1,13 @@
-
-
-const apiURL = "api.openweathermap.org/data/2.5/weather?id={5604473}&appid={159b64909a20d12c8a9f4243af9f627b}";
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=159b64909a20d12c8a9f4243af9f627b";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
   });
