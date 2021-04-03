470b1362d70a5f0cf1fae032bf4d554e672b87ee
jessiemangum16 <jessiemangum16@byui.edu>
2021-04-03 12:39:53 -0600

    update

---
+++ Final Website/js/script-directors.js
@@ -0,0 +1,36 @@
+const requestURL = 'https://jessiemangum16.github.io/Final%20Website/js/directors.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const bus = jsonObject['businesses'];
+    for (let i = 0; i < bus.length; i++ ) {
+        let card = document.createElement('section');
+        let logo = document.createElement('img');
+        let h2 = document.createElement('h2');
+        let p_1 = document.createElement('p');
+        let p_2 = document.createElement('p');
+        let p_3 = document.createElement('p');
+        let p_4 = document.createElement('p');
+
+        logo.setAttribute('src', bus[i].logo);
+        logo.setAttribute('alt', bus[i].name + 'logo');
+        h2.textContent = jsonObject.businesses[0].name;
+        p_1.textContent = bus[i].website;
+        p_2.textContent = bus[i].address1;
+        p_3.textContent = bus[i].address2;
+        p_4.textContent = bus[i].phone;
+
+        card.appendChild(logo);
+        card.appendChild(h2);
+        card.appendChild(p_1);
+        card.appendChild(p_2);
+        card.appendChild(p_3);
+        card.appendChild(p_4);
+
+        document.querySelector('div#local-b').appendChild(card);
+    }
+  });
