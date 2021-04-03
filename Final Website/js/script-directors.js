70761a3417f11d5a2bef46377ee816da35fa74f3
jessiemangum16 <jessiemangum16@byui.edu>
2021-04-03 12:44:34 -0600

    update

--- Final Website/js/script-directors.js
+++ Final Website/js/script-directors.js
@@ -6,31 +6,28 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const bus = jsonObject['businesses'];
-    for (let i = 0; i < bus.length; i++ ) {
+    const b = jsonObject['board'];
+    for (let i = 0; i < b.length; i++ ) {
         let card = document.createElement('section');
-        let logo = document.createElement('img');
+        let img = document.createElement('img');
         let h2 = document.createElement('h2');
         let p_1 = document.createElement('p');
         let p_2 = document.createElement('p');
         let p_3 = document.createElement('p');
-        let p_4 = document.createElement('p');
 
-        logo.setAttribute('src', bus[i].logo);
-        logo.setAttribute('alt', bus[i].name + 'logo');
-        h2.textContent = jsonObject.businesses[0].name;
-        p_1.textContent = bus[i].website;
-        p_2.textContent = bus[i].address1;
-        p_3.textContent = bus[i].address2;
-        p_4.textContent = bus[i].phone;
+        img.setAttribute('src', b[i].photo);
+        img.setAttribute('alt', b[i].name + 'logo');
+        h2.textContent = jsonObject.binesses[0].name;
+        p_2.textContent = b[i].address1;
+        p_3.textContent = b[i].address2;
+        p_4.textContent = b[i].phone;
 
-        card.appendChild(logo);
+        card.appendChild(img);
         card.appendChild(h2);
         card.appendChild(p_1);
         card.appendChild(p_2);
         card.appendChild(p_3);
-        card.appendChild(p_4);
 
-        document.querySelector('div#local-b').appendChild(card);
+        document.querySelector('div#board').appendChild(card);
     }
   });
