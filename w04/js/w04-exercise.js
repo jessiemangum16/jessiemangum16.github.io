e4141dea458e27ff077838c5986ab9b3941555a3
jessiemangum16 <jessiemangum16@byui.edu>
2021-05-14 17:46:52 -0600

    update

--- w04/js/w04-exercise.js
+++ w04/js/w04-exercise.js
@@ -32,19 +32,5 @@ function calculate(){
     document.getElementById("distance-2").innerText = "Vector distance from origin: " + outputDistance2.toFixed(2);
 }
 
-function sub(){
-    const x1 = parseFloat(document.getElementById("x-1").value);
-    const y1 = parseFloat(document.getElementById("y-1").value);
-    const x2 = parseFloat(document.getElementById("x-2").value);
-    const y2 = parseFloat(document.getElementById("y-2").value);
-
-    let outputAdd = new Vec(x1, y1).minus(new Vec(x2, y2));
-    let outputDistance1 = new Vec(x1, y1).length;
-    let outputDistance2 = new Vec(x2, y2).length;
-
-    document.getElementById("sub-div").innerText = "( " + outputAdd.x + ", " + outputAdd.y + " )";
-    document.getElementById("distance-1").innerText = "Distance from origin: " + outputDistance1.toFixed(2);
-    document.getElementById("distance-2").innerText = "Distance from origin: " + outputDistance2.toFixed(2);
-}
 
 
