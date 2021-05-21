9a6fba5645d8d2c77b8d698c9d7666fd94d569c1
jessiemangum16 <jessiemangum16@byui.edu>
2021-05-20 19:15:50 -0600

    update

--- w05/js/w05-team-hikes.js
+++ w05/js/w05-team-hikes.js
@@ -1,100 +1,144 @@
-//create an array of hikes
+
 const hikeList = [
-    {
-      name: "Bechler Falls",
-      imgSrc: "falls.jpg",
-      imgAlt: "Image of Bechler Falls",
-      distance: "3 miles",
-      difficulty: "Easy",
-      description:
-        "Beautiful short hike along the Bechler river to Bechler Falls",
-      directions:
-        "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead."
-    },
-    {
-      name: "Teton Canyon",
-      imgSrc: "falls.jpg",
-      imgAlt: "Image of Bechler Falls",
-      distance: "3 miles",
-      difficulty: "Easy",
-      description: "Beautiful short (or long) hike through Teton Canyon.",
-      directions:
-        "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
-    },
-    {
-      name: "Denanda Falls",
-      imgSrc: "falls.jpg",
-      imgAlt: "Image of Bechler Falls",
-      distance: "7 miles",
-      difficulty: "Moderate",
-      description:
-        "Beautiful hike through Bechler meadows river to Denanda Falls",
-      directions:
-        "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead."
-    }
-  ];
-  
-  const imgBasePath = "https://byui-cit.github.io/cit261/examples/";
-
-  export default class Hikes {
-    //build and hide the back button
-    constructor(elementId) {
-      this.parentElement = document.getElementById(elementId);
-      this.backButton = this.buildBackButton();
-    }
-    getAllHikes() {
-      return hikeList;
-    }
-    
-    getHikeByName(hikeName) {
-      return this.getAllHikes().find(hike => hike.name === hikeName);
-    }
-    
-    showHikeList() {
-      const hikeListElement = document.getElementById("hikes");
-      hikeListElement.innerHTML = "";
-      renderHikeList(hikeList, hikeListElement);
-    }
-
-    showOneHike(hikeName) {
-      // show one hike with full details
-    }
-
-    addHikeListener() {
-      //loop through each hike and attach a listener to display full hike info
-    }
-    buildBackButton() {
-      const backButton = document.createElement("button");
-      // build back button
-      return backButton;
-    }
+  {
+    name: "Bechler Falls",
+    imgSrc: "falls.jpg",
+    imgAlt: "Image of Bechler Falls",
+    distance: "3 miles",
+    difficulty: "Easy",
+    description:
+      "Beautiful short hike along the Bechler river to Bechler Falls",
+    directions:
+      "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead."
+  },
+  {
+    name: "Teton Canyon",
+    imgSrc: "falls.jpg",
+    imgAlt: "Image of Bechler Falls",
+    distance: "3 miles",
+    difficulty: "Easy",
+    description: "Beautiful short (or long) hike through Teton Canyon.",
+    directions:
+      "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
+  },
+  {
+    name: "Denanda Falls",
+    imgSrc: "falls.jpg",
+    imgAlt: "Image of Bechler Falls",
+    distance: "7 miles",
+    difficulty: "Moderate",
+    description:
+      "Beautiful hike through Bechler meadows river to Denanda Falls",
+    directions:
+      "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead."
   }
-  
-  function renderHikeList(parent, hikes) {
-    hikes.forEach(hike => {
-      parent.appendChild(renderOneHikeLight(hike));
+];
+
+const imgBasePath = "//byui-cit.github.io/cit261/examples/";
+
+export default class Hikes {
+  constructor(elementId) {
+    this.parentElement = document.getElementById(elementId);
+    // build back button and hide it
+    this.backButton = this.buildBackButton();
+  }
+
+  getAllHikes() {
+    return hikeList;
+  }
+
+  getHikeByName(hikeName) {
+    return this.getAllHikes().find(hike => hike.name === hikeName);
+  }
+
+  showHikeList() {
+    const hikeListElement = document.getElementById("hikes");
+    hikeListElement.innerHTML = "";
+    renderHikeList(hikeList, hikeListElement);
+
+    this.addHikeListener();
+    //hide back button
+  }
+
+  showOneHike(hikename) {
+    //display one hike
+    const hike = this.getHikeByName(hikeName);
+    console.log(hikename);
+    const hikeListElement = document.getElementById("hikes");
+    hikeListElement.innerHTML = "";
+    
+    
+    hikeListElement.appendChild(renderOneHikeLong(hike));
+
+    //show hide button
+  }
+
+  addHikeListener() {
+
+    const childrenArray = Array.from(this.parentElement.children);
+    childrenArray.forEach(child => {
+      child.addEventListener('click', e => {
+        // why currentTarget instead of target?
+        this.showOneHike(e.currentTarget.dataset.name);
+      });
     });
   }
 
-  function renderOneHikeLight(hike) {
-    const item = document.createElement("li");
-    item.innerHTML = ` <h2>${hike.name}</h2>
-    <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
-    <div>
-            <div>
-                <h3>Distance</h3>
-                <p>${hike.distance}</p>
-            </div>
-            <div>
-                <h3>Difficulty</h3>
-                <p>${hike.difficulty}</p>
-            </div>
-    </div>`;
-    return item;
+  buildBackButton() {
+    const backButton = document.createElement("button");
+    //build back button
+    return backButton;
   }
-  
-  function renderOneHikeFull(hike) {
-    const item = document.createElement("li");
-    //stretch goal 1 create one hike
-    return item;
-  }
+}
+
+function renderHikeList(hikes, parent) {
+    hikes.forEach(hike => {
+      parent.appendChild(renderOneHikeShort(hike));
+    });
+}
+
+function renderOneHikeShort(hike) {
+  const itemShort = document.createElement("li");
+
+  itemShort.classList.add("short");
+  itemShort.innerHTML = ` <h2>${hike.name}</h2>
+        <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
+        <div class="hike-data">
+                <div>
+                    <h3>Distance</h3>
+                    <p>${hike.distance}</p>
+                </div>
+                <div>
+                    <h3>Difficulty</h3>
+                    <p>${hike.difficulty}</p>
+                </div>
+        </div>`;
+  return itemShort;
+}
+
+function renderOneHikeLong(hike) {
+  const itemLong = document.createElement("li");
+
+  itemLong.classList.add("long");
+  itemLong.innerHTML = ` <h2>${hike.name}</h2>
+        <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
+        <div class="hike-data">
+                <div>
+                    <h3>Distance</h3>
+                    <p>${hike.distance}</p>
+                </div>
+                <div>
+                    <h3>Difficulty</h3>
+                    <p>${hike.difficulty}</p>
+                </div>
+                <div>
+                    <h3>Description</h3>
+                    <p>${hike.description}</p>
+                </div>    
+        </div>
+        <div class="hike-directions">
+                    <h3>Directions</h3>
+                    <p>${hike.directions}</p>
+        </div>`;
+  return itemLong;
+}
