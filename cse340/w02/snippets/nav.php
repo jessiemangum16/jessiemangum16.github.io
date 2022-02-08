8d9688ebd26184f120005f25e3e3b3e1e0d63435
jessiemangum16 <jessiemangum16@byui.edu>
2022-02-08 10:00:45 -0700

    update

--- cse340/w02/snippets/nav.php
+++
@@ -1,8 +0,0 @@
-<ul>
-    <li><a href="#">Home</a></li>
-    <li><a href="#">Classic</a></li>
-    <li><a href="#">Sports</a></li>
-    <li><a href="#">SUV</a></li>
-    <li><a href="#">Tucks</a></li>
-    <li><a href="#">Used</a></li>
-</ul>
--- cse340/w06/snippets/header.php
+++
@@ -1,4 +0,0 @@
-<img src="/phpmotors/images/site/logo.png" alt="PHP Motors Logo">
-
-<a class="account" href="/phpmotors/accounts/index.php?action=signin">My Account</a>
-
