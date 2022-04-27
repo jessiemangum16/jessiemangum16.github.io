73cc6870fa31864319bfbf3dee03ed77c4e373bf
jessiemangum16 <jessiemangum16@byui.edu>
2022-04-26 20:51:44 -0600

    update

--- cse341-new/personal/routes/index.js
+++ cse341-new/personal/routes/index.js
@@ -1,9 +1,7 @@
 const routes = require('express').Router();
 
 
-routes.get('/', (req, res) => {
-    res.send('Jessie Mangum')
-  })
+routes.use('/contacts', require("./contacts"))
 
-  module.exports = routes;
+module.exports = routes;
   
--- cse341-new/personal/server.js
+++ cse341-new/personal/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongodb = require('./DB/connection');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'))
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+mongodb.initDb((err, mongodb) => {
+  if(err) {
+    console.log(err);
+  }else{
+    app.listen(port);
+    console.log(`Example app listening on port ${port}`);
+  }
+});
