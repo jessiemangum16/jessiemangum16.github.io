5349f65c34c90157bda033360c274174c0cc0bf9
jessiemangum16 <jessiemangum16@byui.edu>
2022-05-25 19:16:11 -0600

    update

--- cse341-new/personal-2/routes/events.js
+++ cse341-new/personal-2/routes/events.js
@@ -9,83 +9,108 @@ routes.use(bodyParser.json());
 
 //Get ALL
 routes.get("/", (req, res) => {
-  dbConnection
+  try{
+    dbConnection
     .getCollectionEvents()
     .find()
     .toArray()
     .then((documents) => {
       res.status(200).json(documents);
       console.log("returned all");
-    });
+    }).catch((error) => console.error(error));
+  } catch (err){
+    res.status(500).json(err);
+  }
+  
+    
 });
 
 //Get ONE
 routes.get("/:eventName", (req, res) => {
-  const eventName = req.params.eventName;
-  const results = dbConnection.getCollectionEvents().find({ eventName: eventName });
+  try{
+    const eventName = req.params.eventName;
 
-  results.toArray().then((documents) => {
-    res.status(200).json(documents[0]);
-    console.log(`returned ${req.params.eventName}`);
-  });
+    const results = dbConnection.getCollectionEvents().find({ eventName: eventName });
+
+    console.log(results);
+
+    results.toArray().then((documents) => {
+      res.status(200).json(documents[0]);
+      console.log(`returned ${req.params.eventName}`);
+    }).catch((error) => console.error(error));
+  } catch (err){
+    res.status(500).json(err);
+  }
 });
 
 //Add new 
 routes.post("/", (req, res) => {
-  const event = {
-    eventName: req.body.eventName,
-    date: req.body.date,
-    eventHost: req.body.eventHost,
-    startTime: req.body.startTime,
-    endTime: req.body.endTime,
-    location: req.body.location,
-    comments: req.body.comments 
-  };
+  try{
+    const event = {
+      eventName: req.body.eventName,
+      date: req.body.date,
+      eventHost: req.body.eventHost,
+      startTime: req.body.startTime,
+      endTime: req.body.endTime,
+      location: req.body.location,
+      comments: req.body.comments 
+    };
 
-  dbConnection
-    .getCollectionEvents()
-    .insertOne(event)
-    .then((result) => {
-      res.status(200).json(result[0]);
-      console.log(result);
-      console.log(event);
-    })
-    .catch((error) => console.error(error));
+    dbConnection
+      .getCollectionEvents()
+      .insertOne(event)
+      .then((result) => {
+        res.status(200).json(result[0]);
+        console.log(result);
+        console.log(event);
+      }).catch((error) => console.error(error));
+  } catch (err){
+    res.status(500).json(err);
+    console.log("oops error!");
+  }
 });
 
 //Update
 routes.put("/:eventName", (req, res) => {
-  const eventName = req.params.eventName;
+  try{
+    const eventName = req.params.eventName;
 
-  const event = {
-    eventName: req.body.eventName,
-    date: req.body.date,
-    eventHost: req.body.eventHost,
-    startTime: req.body.startTime,
-    endTime: req.body.endTime,
-    location: req.body.location,
-    comments: req.body.comments 
-  };
+    const event = {
+      eventName: req.body.eventName,
+      date: req.body.date,
+      eventHost: req.body.eventHost,
+      startTime: req.body.startTime,
+      endTime: req.body.endTime,
+      location: req.body.location,
+      comments: req.body.comments 
+    };
 
-  const results = dbConnection
-    .getCollectionEvents()
-    .replaceOne({ eventName: eventName }, event);
+    const results = dbConnection
+      .getCollectionEvents()
+      .replaceOne({ eventName: eventName }, event);
 
-  results.then((documents) => {
-    res.status(200).json(documents[0]);
-    console.log(`Updated ${req.params.eventName}`);
-  });
+    results.then((documents) => {
+      res.status(200).json(documents[0]);
+      console.log(`Updated ${req.params.eventName}`);
+    }).catch((error) => console.error(error));
+  } catch (err){
+    res.status(500).json(err);
+  }
 });
 
 //Delete
 routes.delete("/:eventName", (req, res) => {
-  const eventName = req.params.eventName;
-  const results = dbConnection.getCollectionEvents().deleteOne({ eventName: eventName });
+  try{
+    const eventName = req.params.eventName;
+    const results = dbConnection.getCollectionEvents().deleteOne({ eventName: eventName });
 
-  results.then((documents) => {
-    res.status(200).json(documents[0]);
-    console.log(`Deleted ${req.params.eventName}`);
-  });
+    results.then((documents) => {
+      res.status(200).json(documents[0]);
+      console.log(`Deleted ${req.params.eventName}`);
+    }).catch((error) => console.error(error));
+  } catch (err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = routes;
--- cse341-new/personal-2/routes/location.js
+++ cse341-new/personal-2/routes/location.js
@@ -9,83 +9,102 @@ routes.use(bodyParser.json());
 
 //Get ALL locations
 routes.get("/", (req, res) => {
-  dbConnection
-    .getCollectionLocation()
-    .find()
-    .toArray()
-    .then((documents) => {
-      res.status(200).json(documents);
-      console.log("returned all locations");
-    });
+  try{
+    dbConnection
+      .getCollectionLocation()
+      .find()
+      .toArray()
+      .then((documents) => {
+        res.status(200).json(documents);
+        console.log("returned all locations");
+      }).catch((error) => console.error(error));
+  } catch (err){
+    res.status(500).json(err);
+  }
 });
 
 //Get ONE location by name
 routes.get("/:locationName", (req, res) => {
-  const locationName = req.params.locationName;
-  const results = dbConnection.getCollectionLocation().find({ locationName: locationName });
+  try{
+    const locationName = req.params.locationName;
+    const results = dbConnection.getCollectionLocation().find({ locationName: locationName });
 
-  results.toArray().then((documents) => {
-    res.status(200).json(documents[0]);
-    console.log(`returned location ${req.params.locationName}`);
-  });
+    results.toArray().then((documents) => {
+      res.status(200).json(documents[0]);
+      console.log(`returned location ${req.params.locationName}`);
+    }).catch((error) => console.error(error));
+  } catch (err){
+    res.status(500).json(err);
+  }
 });
 
 //Add new location
 routes.post("/", (req, res) => {
-  const location = {
-    locationName: req.body.locationName,
-    streetAddress: req.body.streetAddress,
-    city: req.body.city,
-    state: req.body.state,
-    zipCode: req.body.zipCode,
-    webAddress: req.body.webAddress,
-    phoneNum: req.body.phoneNum
-  };
+  try{
+    const location = {
+      locationName: req.body.locationName,
+      streetAddress: req.body.streetAddress,
+      city: req.body.city,
+      state: req.body.state,
+      zipCode: req.body.zipCode,
+      webAddress: req.body.webAddress,
+      phoneNum: req.body.phoneNum
+    };
 
-  dbConnection
-    .getCollectionLocation()
-    .insertOne(location)
-    .then((result) => {
-      res.status(200).json(result[0]);
-      console.log(result);
-      console.log(location);
-    })
-    .catch((error) => console.error(error));
+    dbConnection
+      .getCollectionLocation()
+      .insertOne(location)
+      .then((result) => {
+        res.status(200).json(result[0]);
+        console.log(result);
+        console.log(location);
+      }).catch((error) => console.error(error));
+  } catch (err){
+    res.status(500).json(err);
+  }
 });
 
 //Update
 routes.put("/:locationName", (req, res) => {
-  const locationName = req.params.locationName;
+  try{
+    const locationName = req.params.locationName;
 
-  const location = {
-    locationName: req.body.locationName,
-    streetAddress: req.body.streetAddress,
-    city: req.body.city,
-    state: req.body.state,
-    zipCode: req.body.zipCode,
-    webAddress: req.body.webAddress,
-    phoneNum: req.body.phoneNum
-  };
+    const location = {
+      locationName: req.body.locationName,
+      streetAddress: req.body.streetAddress,
+      city: req.body.city,
+      state: req.body.state,
+      zipCode: req.body.zipCode,
+      webAddress: req.body.webAddress,
+      phoneNum: req.body.phoneNum
+    };
 
-  const results = dbConnection
-    .getCollectionLocation()
-    .replaceOne({ locationName: locationName }, location);
+    const results = dbConnection
+      .getCollectionLocation()
+      .replaceOne({ locationName: locationName }, location);
 
-  results.then((documents) => {
-    res.status(200).json(documents[0]);
-    console.log(`Updated ${req.params.locationName}`);
-  });
+    results.then((documents) => {
+      res.status(200).json(documents[0]);
+      console.log(`Updated ${req.params.locationName}`);
+    }).catch((error) => console.error(error));
+  } catch (err){
+    res.status(500).json(err);
+  }
 });
 
 //Delete
 routes.delete("/:locationName", (req, res) => {
-  const locationName = req.params.locationName;
-  const results = dbConnection.getCollectionLocation().deleteOne({ locationName: locationName });
+  try{
+    const locationName = req.params.locationName;
+    const results = dbConnection.getCollectionLocation().deleteOne({ locationName: locationName });
 
-  results.then((documents) => {
-    res.status(200).json(documents[0]);
-    console.log(`Deleted ${req.params.locationName}`);
-  });
+    results.then((documents) => {
+      res.status(200).json(documents[0]);
+      console.log(`Deleted ${req.params.locationName}`);
+    }).catch((error) => console.error(error));
+  } catch (err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = routes;
